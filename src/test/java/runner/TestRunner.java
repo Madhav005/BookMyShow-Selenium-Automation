ca1811b43ec08a29948874c5189916d5211a5626
Madhav005 <madhavmathi142k5@gmail.com>
2026-02-05 11:28:54 +0530

    project structure

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,13 +1,5 @@
 package runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+public class TestRunner {
 
-@CucumberOptions(
-	    features = "src/test/resources/features/02_DataTable.feature",
-	    glue = {"stepdefinitions"},
-	    plugin = {"pretty","html:target/report.html"}
-	)
-
-	public class TestRunner extends AbstractTestNGCucumberTests {
-	}
+}
